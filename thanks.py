2b8fefb3dac7504d7b557371e0c19ea7289a839d
Christopher Lycan <96343159+777388@users.noreply.github.com>
2024-04-01 19:11:53 -0700

    Add files via upload

---
+++ thanks.py
@@ -0,0 +1,20 @@
+import sys
+import os
+#import jack
+import jedi
+#import farmhash
+import hashlib
+import genericpath
+try:
+    sys.argv[1] = os.environ
+    #jack.Client(sys.argv[1])
+    jedi.settings(True)
+    #hashlib.new(farmhash.fingerprint128)
+    set.update(genericpath)
+except:
+    try:
+        genericpath
+    except:
+        hashlib.new(genericpath)
+        #farmhash
+        genericpath
